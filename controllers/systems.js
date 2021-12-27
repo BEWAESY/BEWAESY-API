d18558af4a872ab45e1b26bdbf3fd7ed3a4b4574
Johannes <johannes.kuerz@gmx.de>
2021-12-27 12:29:26 +0100

    Worked on shouldWater controller

--- controllers/systems.js
+++ controllers/systems.js
@@ -1,10 +1,30 @@
 var connection = require("../modules/mysql");
 
 
-exports.water = function(req, res, next) {
-    connection.query("SELECT * FROM users WHERE id = 2", function(err, rows, fields) {
+exports.shouldWater = function(req, res, next) {
+    let id = req.query.id;
+    let key = req.query.key;
+
+    connection.query("SELECT * FROM systems WHERE id = ?", [id], function(err, rows, fields) {
+        if (err) throw err;
+
+        if (!rows.length) {
+            res.send("Doesn't exist!");
+            return;
+        }
+
+        res.send(rows);
+    })
+
+
+
+    /*connection.query("SELECT * FROM users WHERE id = 2", function(err, rows, fields) {
         if (err) throw err;
 
         res.send(rows[0]);
-    })
+    })*/
+}
+
+exports.wateredPlant = function(req, res, next) {
+    res.send("NOT IMPLEMENTED!");
 }
--- routes/systems.js
+++ routes/systems.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var system_controller = require("../controllers/systems")
 
-router.get("/water", system_controller.water);
+router.get("/should-water", system_controller.shouldWater);
+router.get("/watered-plant", system_controller.wateredPlant);
 
 module.exports = router;
