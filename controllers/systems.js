54ad36088c02c7206ae5c9046c1d2ff1e39523f0
Johannes <johannes.kuerz@gmx.de>
2021-12-29 11:46:34 +0100

    Revert "Update systems.js"
    This reverts commit 129fc7cfe363c56aefdbd3a4949ebc9aa06d71c9.

--- controllers/systems.js
+++ controllers/systems.js
@@ -25,23 +25,7 @@ exports.shouldWater = function(req, res, next) {
         connection.query("SELECT * FROM wateringevents WHERE systemid = ?", [id], function(err2, rows2, fields2) {
             if (err2) throw err2;
     
-            for (let i of rows2) {
-                if (i.eventTrigger == "time") {
-                    let datetime = new Date();
-
-                    console.log(datetime.getHours(), datetime.getMinutes());
-                    console.log(i.triggerValue1.substring(0, 2), i.triggerValue1.substring(3, 5));
-
-                    // Check if time is the same
-                    if (i.triggerValue1.substring(0, 2) == datetime.getHours() && i.triggerValue1.substring(3, 5) == datetime.getMinutes()) {
-                        console.log("Water plant!");
-                    } else {
-                        console.log("Don't water plant!");
-                    }
-                }
-            }
-
-            res.send(rows2);
+            res.json(rows2);
         })
     });
 }
