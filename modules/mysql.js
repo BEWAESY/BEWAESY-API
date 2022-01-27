250e4f7841077bed318fbbbacf72df574155a773
Johannes <johannes.kuerz@gmx.de>
2022-01-27 15:43:47 +0100

    created environment variables for MySQL credentials

--- modules/mysql.js
+++ modules/mysql.js
@@ -1,10 +1,10 @@
 
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'bewaesy'
+  host     : process.env.DBHOSTNAME,
+  user     : process.env.DBUSER,
+  password : process.env.DBPASSWORD,
+  database : process.env.DBDATABASE
 });
 
 connection.connect();
